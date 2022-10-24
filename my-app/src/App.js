e3fac1d0647211355c61479841a91ee4d691ea21
Chewie94 <mykhailo.butkevych@gmail.com>
2022-10-24 16:07:32 +0400

    feat: simple redux

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+
+function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash});
+  }
+  
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash});
+  }
+
+  return (
+    <div className="App">
+      <div style={{ fontSize: "3rem" }}>{cash}</div>
+      <div style={{ display: 'flex' }}>
+        <button onClick={() => addCash(Number(prompt()))}>Add money</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get money</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
