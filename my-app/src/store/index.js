6afe0529c218772cc456e72420a8d89f3916257d
Chewie94 <mykhailo.butkevych@gmail.com>
2022-10-24 17:34:58 +0400

    feat: add combineReducers, chrome extension support

---
+++ my-app/src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore , combineReducers} from 'redux';
+import {cashReducer} from './cashReducer';
+import {customerReducer} from './customerReducer';
+
+const rootReducer = combineReducers({
+    cash: cashReducer,
+    customers: customerReducer,
+})
+
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
