5ae85ea47cde901d4895df3f430d7b844108d1f8
Chewie94 <mykhailo.butkevych@gmail.com>
2022-10-25 21:28:13 +0400

    feat: finished

---
+++ redux-ts-app/src/store/action-creators/index.ts
@@ -0,0 +1,8 @@
+import * as UserActionCreators from './user'
+import * as TodoActionCreators from './todo'
+
+export const ActionCreators = {
+    ...TodoActionCreators,
+    ...UserActionCreators
+};
+
